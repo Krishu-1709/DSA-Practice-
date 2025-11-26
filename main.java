f1be713dbe20496ccfe81fd7a27b549770aa3fa0
Krishu-1709 <Krishnacodes007@gmail.com>
2025-11-26 13:31:39 +0530

    Again commiting unnecessary things instead of going to college.

---
+++ main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void Main(String[] args) {
+//        System.out.println("Hi this is krishna");
+//        int a[] = new int[5];
+//        System.out.println(a[4]);
+        System.out.println("Hello world");
+
+    }
+}
