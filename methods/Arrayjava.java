bec67f090833f0b8ffe76db5824d82f61ed3d788
Krishu-1709 <Krishnacodes007@gmail.com>
2025-12-17 06:21:26 +0530

    Again commiting unnecessary things instead of going to college.

---
+++ methods/Arrayjava.java
@@ -0,0 +1,20 @@
+public class Arrayjava {
+    public static void main(String[] args) {
+
+
+        int[] ecx = {-12,32,3,32,1,3,47,65,654,6,546,54};
+        ques(ecx);
+
+
+
+    }
+
+
+
+    public static void print(int[]arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println(" ");
+    }
+}
