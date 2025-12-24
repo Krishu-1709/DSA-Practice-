82ffd43078fa8a2c9991e27f3c4c7d991120197b
Krishu-1709 <Krishnacodes007@gmail.com>
2025-12-24 10:35:46 +0530

    Again commiting unnecessary things instead of going to college.

--- methods/Arrayjava.java
+++ methods/Arrayjava.java
@@ -2,19 +2,117 @@ public class Arrayjava {
     public static void main(String[] args) {
 
 
-        int[] ecx = {-12,32,3,32,1,3,47,65,654,6,546,54};
-        ques(ecx);
+        int[] ecx = {-1,2,3,1,2,3,4,234,5,5,43,456543,5,43,5,432,5,43,24,543,2,454};
+        RevPartOfArray(ecx,2,9);
 
 
 
     }
 
+    public static void RevPartOfArray(int[]arr,int i,int j) {
+        int b = i;
+        int l = j;
+        while(j>i){
+            swap(arr, i,j);// this reverses a part of an array
+            i++;
+            j--;
+        }
+        for (int k = b; k <= l; k++) {
+            System.out.print(arr[k]+" ");
+
+        }
+
+    }
+
+    public static void Revarray(int[]arr) {
+        int n = arr.length-1;// reverses an array
+        int j = 0;
+
+        while(n>j){
+            swap(arr, j,n);
+            j++;
+            n--;
+        }
+
+        print(arr);
 
 
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];// swaps elements of array
+        arr[j] = temp;
+    }
+
+
+    public static void findSecondMaxInt(int[] arr) {
+        int max = Integer.MIN_VALUE;;
+        for (int i = 0; i < arr.length; i++) {
+
+            if(arr[i] > max){
+                max = arr[i];// finds second max element
+            }
+
+        }
+        int Smax = Integer.MIN_VALUE;;
+        for (int j = 0; j < arr.length; j++) {
+
+            if(arr[j] > Smax && arr[j]< max){
+                Smax = arr[j];
+            }
+
+        }
+        System.out.println(max);
+        System.out.println(Smax);
+
+    }
+    public static void findMaxInt(int[] arr) {
+        int max = Integer.MIN_VALUE;;
+        for (int i = 0; i < arr.length; i++) {
+// finds largest integer in array
+            if(arr[i] > max){
+                max = arr[i];
+            }
+
+        }
+        System.out.println(max);
+
+    }
+
+    public static void linearSearch(int[]arr , int target) {print(arr);
+// this searches element of array
+        boolean found = false;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                System.out.println("We found target (" + target + ") at " + i + " index.");
+                found = true;
+                break; // optional: remove this if you want all occurrences
+            }
+        }
+
+        if (!found) {
+            System.out.println("Target (" + target + ") not found in the array.");
+        }
+    }
+    
+
+
+    public static void productOfArrays(int[]arr) {
+        int i =1;
+        for (int j = 0; j < arr.length; j++) {
+            i = i*arr[j];
+//prints product of all element of array
+        }
+        print(arr);
+        System.out.println(i);
+
+    }
     public static void print(int[]arr) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]+" ");
-        }
-        System.out.println(" ");
+        }// This prints all the elements of array
+        System.out.print(" ");
     }
 }
